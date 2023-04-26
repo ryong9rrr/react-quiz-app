8f3f1f5d37abf78d28ff9dea675b8626dad04557
yong <tkddbs1644@gmail.com>
2023-04-27 00:39:55 +0900

    feat: NotFoundPage 스타일링, meta title 설정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,23 @@
 import React, { Suspense } from 'react'
 import styled from '@emotion/styled'
+import { useNavigate } from 'react-router-dom'
+
+import * as Atom from '@/components/atom'
+import { routeTable } from '@/routes'
 
 const NotFound = React.lazy(() => import('@/components/NotFound'))
 
 export default function NotFoundPage() {
+  const navigate = useNavigate()
+
   return (
     <>
       <Suspense>
         <NotFound />
       </Suspense>
       <Texts>
-        <div>존재하지 않는 페이지입니다.</div>
-        <button>홈으로</button>
+        <Atom.Text size="lg">존재하지 않는 페이지입니다.</Atom.Text>
+        <Atom.Button onClick={() => navigate(routeTable.HOME.path)}>홈으로</Atom.Button>
       </Texts>
     </>
   )
