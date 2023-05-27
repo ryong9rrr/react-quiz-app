0d24cc73123500cc392b9171c33bba78923ba246
yong <tkddbs1644@gmail.com>
2023-05-27 17:48:09 +0900

    refactor: App외부의 provider를 App 내부로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import { setupStore } from './store'
 import { Routes } from './pages/Routes'
 import { PageLayout } from './pages/PageLayout'
 
+const store = setupStore()
+const persistor = persistStore(store)
+
 function App() {
   return (
-    <PageLayout>
-      <Routes />
-    </PageLayout>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <PageLayout>
+          <Routes />
+        </PageLayout>
+      </PersistGate>
+    </Provider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-
 import App from './App'
-import { setupStore } from './store'
-
-const store = setupStore()
-const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </BrowserRouter>,
 )
